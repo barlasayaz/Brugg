ee61d71b4fb6f1b5f5521d1759158b0462fa0fd9
PoyrazMac <dpoyraz@hotmail.com>
2019-06-20 01:00:13 +0300

    fix

--- src/app/my-data/my-data-edit/my-data-edit.page.ts
+++ src/app/my-data/my-data-edit/my-data-edit.page.ts
@@ -19,14 +19,13 @@ import { UserdataService } from '../../services/userdata';
 
 export class MyDataEditPage {
   public params: any;
-  public pid:number =0;
-
-  public edit: any = {bid:{}, role_set:[]};
-  public Farbe:string="#000fff";
-  public editType:number=0;
-  public role:number=0;
-  public role_nr:number=0;
-  public role_set:any = {
+  public pid: number;
+  public edit: any = {bid: {}, role_set: []};
+  public Farbe: string;
+  public editType: number;
+  public role: number;
+  public role_nr: number;
+  public role_set: any = {
     edit_customer: false,
     edit_membership: false,
     edit_rights: false,
@@ -35,141 +34,148 @@ export class MyDataEditPage {
     edit_contact_persons: false,
     licensee: 5
   };
-  public setRights:boolean = false;
-  public emailToFind:string = "";
-  public viewNewMode:number = 0;
-  public viewNewObj:any = {first_name:"",last_name:"" };
-  
+  public setRights: boolean;
+  public emailToFind: string;
+  public viewNewMode: number;
+  public viewNewObj: any = {first_name: '', last_name: '' };
+
   constructor(public navCtrl: NavController, 
               public userdata: UserdataService, 
               public navParams: NavParams, 
               public translate: TranslateService,
               private api : ApiService,
               public viewCtrl: ModalController) {
-
-              this.pid     = parseInt( this.navParams.get("pid") );
-              this.role    = parseInt( this.navParams.get("role") );
-              this.role_nr = parseInt( this.navParams.get("role_nr") );
-              this.editType = parseInt( this.navParams.get("editType") );
-              if(this.role ==2){
-                this.role_set.edit_customer = false;
-                this.role_set.edit_membership = false;
-                this.role_set.edit_rights = false;
-                this.role_set.edit_products = false;
-                this.role_set.check_products = false;
-                this.role_set.edit_contact_persons = false;
-                this.role_set.licensee = this.userdata.licensee
-              }
-              this.editData(this.pid);
+    console.log('my-data-edit.ts')
+    this.pid = 0;
+    this.Farbe = '#000fff';
+    this.editType = 0;
+    this.role = 0;
+    this.role_nr = 0;
+    this.setRights = false;
+    this.emailToFind = '';
+    this.viewNewMode = 0;
+    this.pid = parseInt( this.navParams.get('pid') );
+    this.role = parseInt( this.navParams.get('role') );
+    this.role_nr = parseInt( this.navParams.get('role_nr') );
+    this.editType = parseInt( this.navParams.get('editType') );
+    if (this.role == 2) {
+      this.role_set.edit_customer = false;
+      this.role_set.edit_membership = false;
+      this.role_set.edit_rights = false;
+      this.role_set.edit_products = false;
+      this.role_set.check_products = false;
+      this.role_set.edit_contact_persons = false;
+      this.role_set.licensee = this.userdata.licensee;
+    }
+    this.editData(this.pid);
   }
 
   dismiss() {
-    this.viewCtrl.dismiss({'update':false});
-  }  
+    this.viewCtrl.dismiss({'update': false});
+  }
 
-  editData(pid:number) {
-    console.info("editData() ", pid,  this.userdata.profile);
+  editData(pid: number) {
+    console.log('editData() ', pid,  this.userdata.profile);
     this.setRights = false;
-    if(pid != this.userdata.profile){
-      if(this.userdata.role_set.edit_rights) this.setRights = true;
+    if (pid != this.userdata.profile) {
+      if (this.userdata.role_set.edit_rights) { this.setRights = true; }
     }
-    if(pid != 0){
+    if (pid != 0) {
       this.api.pvs4_get_mydata(this.pid).then((result: any) => {
-        console.log('pvs4_get_mydata: ', result["obj"]);
-        this.edit=result["obj"];
+        console.log('pvs4_get_mydata: ', result['obj']);
+        this.edit = result['obj'];
       });
     }
-  } 
+  }
 
-  findEmail(){
-    console.log('findEmail emailToFind:',this.emailToFind);
+  findEmail() {
+    console.log('findEmail emailToFind:', this.emailToFind);
     this.api.pvs4_get_profile(this.emailToFind, 1).then((done: any) => {
-      if(done.amount==0){
+      if (done.amount == 0) {
         this.viewNewMode = 1;
-      } else{
+      } else {
         this.viewNewMode = 2;
         this.viewNewObj  = done.obj;
         this.viewNewObj.last_name  = done.bid.last_name;
         this.viewNewObj.first_name = done.bid.first_name;
-      }      
-    });    
-  } 
-
-  addUser(){
-    console.log('addUser:',this.viewNewObj, this.role);
-    this.api.pvs4_get_profile(this.viewNewObj.email).then((done: any) => { 
-      done = done.obj;
-
-      if(this.role==1) {
-        done.system_role = JSON.parse(done.system_role );
-        done.system_role[done.system_role.length]   = this.role_set;
-        done.system_role = JSON.stringify(done.system_role ); 
       }
-      if(this.role==2) {
-        done.licensee_role = JSON.parse(done.licensee_role ); 
-        let x = done.licensee_role.length;
-        for(let i =0; i<x; i++){
-          if(done.licensee_role[i].licensee == this.role_set.licensee) x = i;
-        }
-        done.licensee_role[x] = this.role_set;
-        done.licensee_role = JSON.stringify(done.licensee_role );         
-      }
-      if(this.role==3){
-        done.customer_role = JSON.parse(done.customer_role ); 
-        done.customer_role[done.customer_role.length] = this.role_set;
-        done.customer_role = JSON.stringify(done.customer_role ); 
-      } 
-
-      this.api.pvs4_set_profile(done).then((done: any) => { 
-          console.log("addUser pvs4_set_profile() ok ");
-          this.viewCtrl.dismiss({'update':true});
-      },
-        err => { // return the error
-          console.error("MyDataEditPage pvs4_set_profile() nok ",err);
-      });         
-    },
-      err => { // return the error
-        console.error("MyDataEditPage pvs4_get_profile() nok ",err)
     });
   }
 
-  updateData( ) {    
-    console.log('updateData this.edit:',this.edit);
-    this.api.pvs4_get_profile(this.edit.email).then((done: any) => { 
+  addUser() {
+    console.log('addUser:', this.viewNewObj, this.role);
+    this.api.pvs4_get_profile(this.viewNewObj.email).then((done: any) => {
       done = done.obj;
 
-      if(this.role==1) {
+      if (this.role == 1) {
+        done.system_role = JSON.parse(done.system_role );
+        done.system_role[done.system_role.length]   = this.role_set;
+        done.system_role = JSON.stringify(done.system_role );
+      }
+      if (this.role == 2) {
+        done.licensee_role = JSON.parse(done.licensee_role ); 
+        let x = done.licensee_role.length;
+        for (let i = 0; i < x; i++) {
+          if (done.licensee_role[i].licensee == this.role_set.licensee) { x = i; }
+        }
+        done.licensee_role[x] = this.role_set;
+        done.licensee_role = JSON.stringify(done.licensee_role );
+      }
+      if (this.role == 3) {
+        done.customer_role = JSON.parse(done.customer_role );
+        done.customer_role[done.customer_role.length] = this.role_set;
+        done.customer_role = JSON.stringify(done.customer_role );
+      } 
+
+      this.api.pvs4_set_profile(done).then((done: any) => {
+          console.log('addUser pvs4_set_profile() ok ');
+          this.viewCtrl.dismiss({'update': true});
+      },
+        err => { // return the error
+          console.error('MyDataEditPage pvs4_set_profile() nok ', err);
+      });
+    },
+      err => { // return the error
+        console.error('MyDataEditPage pvs4_get_profile() nok ', err)
+    });
+  }
+
+  updateData( ) {
+    console.log('updateData this.edit:', this.edit);
+    this.api.pvs4_get_profile(this.edit.email).then((done: any) => {
+      done = done.obj;
+
+      if (this.role == 1) {
         done.system_role = JSON.parse(done.system_role );
         done.system_role[this.role_nr]   = this.edit.role_set;
-        done.system_role = JSON.stringify(done.system_role ); 
+        done.system_role = JSON.stringify(done.system_role );
       }
-      if(this.role==2) {
-        done.licensee_role = JSON.parse(done.licensee_role ); 
+      if (this.role == 2) {
+        done.licensee_role = JSON.parse(done.licensee_role );
         done.licensee_role[this.role_nr] = this.edit.role_set;
-        done.licensee_role = JSON.stringify(done.licensee_role );  
+        done.licensee_role = JSON.stringify(done.licensee_role );
       }
-      if(this.role==3){
-        done.customer_role = JSON.parse(done.customer_role ); 
+      if (this.role == 3) {
+        done.customer_role = JSON.parse(done.customer_role );
         done.customer_role[this.role_nr] = this.edit.role_set;
-        done.customer_role = JSON.stringify(done.customer_role ); 
-      } 
+        done.customer_role = JSON.stringify(done.customer_role );
+      }
 
       done.short_code = this.edit.short_code;
       done.colour = this.edit.colour;
 
-      console.log('updateData pvs4_get_profile():',done);
-      this.api.pvs4_set_profile(done).then((done: any) => { 
-          console.log("updateData pvs4_set_profile() ok ");
-          this.viewCtrl.dismiss({'update':true});
+      console.log('updateData pvs4_get_profile():', done);
+      this.api.pvs4_set_profile(done).then((done: any) => {
+          console.log('updateData pvs4_set_profile() ok ');
+          this.viewCtrl.dismiss({'update': true});
       },
         err => { // return the error
-          console.error("MyDataEditPage pvs4_set_profile() nok ",err);
-      });      
-      
+          console.error('MyDataEditPage pvs4_set_profile() nok ', err);
+      });
     },
       err => { // return the error
-        console.error("MyDataEditPage pvs4_get_profile() nok ",err)
-    });     
+        console.error('MyDataEditPage pvs4_get_profile() nok ', err);
+    });
   }
-  
+
 }
--- src/app/my-data/my-data.page.ts
+++ src/app/my-data/my-data.page.ts
@@ -6,7 +6,6 @@ import { TranslateService } from '@ngx-translate/core';
 import { ApiService } from '../services/api';
 import { ModalController } from '@ionic/angular';
 
-
 /**
  * Generated class for the MyDataPage page.
  *
@@ -20,58 +19,60 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./my-data.page.scss'],
 })
 export class MyDataPage {
-  public lang:string = "";
-  public colleagues:any = []; 
+  public lang: string;
+  public colleagues: any = [];
 
   constructor(public navCtrl: NavController, 
               public apiService: ApiService,
               private translate: TranslateService,
               public userdata: UserdataService,
-              public modalCtrl : ModalController) {
-        this.lang = localStorage.getItem('lang');
-        console.log('ionViewDidLoad MyDataPage',this.userdata, this.userdata.role_set.edit_customer);
-        if((this.userdata.role==1)||(this.userdata.role==2)) this.loadList();
+              public modalCtrl: ModalController) {
+
+    this.lang = '';
+    this.lang = localStorage.getItem('lang');
+    console.log('ionViewDidLoad MyDataPage', this.userdata, this.userdata.role_set.edit_customer);
+    if ((this.userdata.role == 1) || (this.userdata.role == 2)) { this.loadList(); }
+
   }
 
-  loadList(){
-    this.apiService.pvs4_get_colleagues_list(this.userdata.role, this.userdata.role_set ,this.userdata.licensee)
+  loadList() {
+    this.apiService.pvs4_get_colleagues_list(this.userdata.role, this.userdata.role_set , this.userdata.licensee)
     .then((result: any) => {
-      console.log("pvs4_get_colleagues_list result:", result);
-      let k = result["obj"];
-      result["amount"] = parseInt(result["amount"]);
-      if(result["amount"]>0){
+      console.log('pvs4_get_colleagues_list result:', result);
+      let k = result['obj'];
+      result['amount'] = parseInt(result['amount']);
+      if (result['amount'] > 0) {
         for (var i = 0, len = k.length; i < len; i++) {
           let item = k[i];
-          item.id = parseInt(item.id);        
-          if(item.id==this.userdata.profile) continue; 
-          //console.log("item:", item);
+          item.id = parseInt(item.id);
+          if (item.id == this.userdata.profile) { continue; }
+          // console.log("item:", item);
           this.colleagues.push(item);
         }
       }
-     
     });
   }
 
   delLocalAll() {
     console.log('delLocalAll()');
-    this.userdata.delStorage(); 
-    this.navCtrl.navigateForward("/Login");  
+    this.userdata.delStorage();
+    this.navCtrl.navigateForward('/Login');
   }
 
-  mw_test_a(nr:number) {
+  mw_test_a(nr: number) {
     console.log('mw_test():', nr);
     let data = {
       type: nr,
       rev: 1
     }
-    this.apiService.pvs4_api_post("mw_get_list.php", data).then((done: any) => { //return the result
+    this.apiService.pvs4_api_post('mw_get_list.php', data).then((done: any) => { // return the result
         console.log('mw_get_list ok :', done);
     },
       err => { // return the error
         console.log('mw_get_list nok err:', err);
     });
   }
-  mw_test_b(nr:number) {
+  mw_test_b(nr: number) {
     console.log('mw_test():', nr);
     let data = {
       type: nr,
@@ -79,7 +80,7 @@ export class MyDataPage {
       id : 49
     }
 
-    this.apiService.pvs4_api_post("mw_handle_object.php", data).then((done: any) => { //return the result
+    this.apiService.pvs4_api_post('mw_handle_object.php', data).then((done: any) => { // return the result
         console.log('mw_get_list ok :', done);
     },
       err => { // return the error
@@ -87,37 +88,37 @@ export class MyDataPage {
     });
   }
 
-  changeLanguage(){
+  changeLanguage() {
     let x = this.lang;
-    console.info("changeLanguage(): "+x);
-    if((x=="de")||(x=="en")||(x=="fr")||(x=="it")) {
+    console.log('changeLanguage(): ' + x);
+    if ((x == 'de') || (x == 'en') || (x == 'fr') || (x == 'it')) {
       this.translate.use(x);
-      localStorage.setItem('lang', x)
-    }else{
-      this.translate.use("en");
-      localStorage.setItem('lang', x)
+      localStorage.setItem('lang', x);
+    } else {
+      this.translate.use('en');
+      localStorage.setItem('lang', x);
     }
   }
-  
-  async open_mydataedit(pid:number, role:number, role_nr:number,  editType:number=0 ) { 
-    if(this.userdata.role_set.edit_membership==false) {
-      if(this.userdata.profile!=pid) return;
+
+  async open_mydataedit(pid: number, role: number, role_nr: number,  editType: number= 0 ) {
+    if (this.userdata.role_set.edit_membership == false) {
+      if (this.userdata.profile != pid) { return; }
     }
-    
-    if(editType==1) role = this.userdata.role;
-    console.log('open_mydataedit()',pid, role, role_nr, editType);
+
+    if (editType == 1) { role = this.userdata.role; }
+    console.log('open_mydataedit()', pid, role, role_nr, editType);
     const modal =
     await this.modalCtrl.create({
       component: MyDataEditPage,
       componentProps: {
-        pid: pid, role:role, role_nr:role_nr ,editType:editType
+        pid: pid, role: role, role_nr: role_nr , editType: editType
       }
     });
     modal.onDidDismiss().then(data => {
-      console.log('MyDataEditPage onDidDismiss:',data['data']);
-      if(data['data']){
-        if(data['data'].update){
-          this.colleagues= [];
+      console.log('MyDataEditPage onDidDismiss:', data['data']);
+      if (data['data']) {
+        if (data['data'].update) {
+          this.colleagues = [];
           this.loadList();
         }
       }
