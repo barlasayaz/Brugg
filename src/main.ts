914e473c335bd527b6c4f6de0ae8e44b0f2a0234
PoyrazMac <dpoyraz@hotmail.com>
2019-06-30 16:54:38 +0300

    item border fix

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { CommonModule } from '@angular/common';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
-  if(window){
-    window.console.log=function(){};
+  if (window) {
+    window.console.log = function() {};
   }
 }
 
