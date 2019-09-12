ebfc6836085b2bf05ad71e0103472c776a0f1ddd
PoyrazMac <dpoyraz@hotmail.com>
2019-09-12 03:18:38 +0300

    List pages fix

--- src/app/protocol-list/protocol-list.page.ts
+++ src/app/protocol-list/protocol-list.page.ts
@@ -4,7 +4,7 @@ import { ApiService } from '../services/api';
 import { TranslateService } from '@ngx-translate/core';
 import { UserdataService } from '../services/userdata';
 import { TreeTable } from 'primeng/components/treetable/treetable';
-import { TreeNode, MenuItem } from 'primeng/api';
+import { TreeNode, MenuItem, LazyLoadEvent } from 'primeng/api';
 import { ExcelService } from '../services/excel';
 import { PdfExportService } from '../services/pdf-export';
 import { ActivatedRoute } from '@angular/router';
@@ -41,8 +41,8 @@ export class ProtocolListPage implements OnInit {
     public customer_number: any;
     modelChanged: Subject<any> = new Subject<any>();
     public selectedRow: number;
-    readonly rowHeight = 46;
-    readonly rowCount: number = 30;
+    public rowHeight = 26;
+    public rowCount = 55;
 
     public menuItems: MenuItem[] = [
         {
@@ -173,12 +173,19 @@ export class ProtocolListPage implements OnInit {
     onResize(event) {
         // console.log('onResize');
         this.funcHeightCalc();
-     }
+    }
 
-     async loadNodes(event) {
-        if (this.protocolListAll.length > 0) {
-            this.generate_protocolList(event.first, event.first + event.rows, event.sortField, event.sortOrder);
+    async loadNodes(event: LazyLoadEvent) {
+        if (this.totalRecords > 0) {
+            this.generate_protocolList(event.first, event.rows, event.sortField, event.sortOrder);
         }
+
+        // in a production application, make a remote request to load data using state metadata from event
+        // event.first = First row offset
+        // event.rows = Number of rows per page
+        // event.sortField = Field name to sort with
+        // event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+        // filters: FilterMetadata object having field as key and filter value, filter matchMode as value
     }
 
     funcHeightCalc() {
@@ -394,13 +401,20 @@ export class ProtocolListPage implements OnInit {
             });
         }
         this.rowRecords = this.protocolListView.length;
-        let endIndex = end_index;
-        if (end_index > this.protocolListView.length) {
-            endIndex = this.protocolListView.length;
+        this.totalRecords = this.protocolListAll.length;
+
+        console.log('start_index - end_index :', start_index, end_index);
+
+        if (this.rowRecords < 22) {
+            this.rowHeight = 52;
+        } else {
+            this.rowHeight = 26;
         }
-        if (endIndex > 0) {
-            this.protocolListView = this.protocolListView.slice(start_index, endIndex);
+
+        if (this.rowRecords > this.rowCount) {
+            this.protocolListView = this.protocolListView.slice(start_index, (start_index + end_index));
         }
+
         if (this.protocolListView.length > 0) {
             if (this.isFilterOn()) {
                 this.menuItems[5].items[0]['disabled'] = false;
@@ -415,7 +429,6 @@ export class ProtocolListPage implements OnInit {
             this.menuItems[5].items[2]['disabled'] = true;
         }
 
-        this.totalRecords = this.protocolListAll.length;
         let progressBar;
         if (this.totalRecords > 0 ) {
             progressBar = Math.round(this.rowRecords * 100 / this.totalRecords);
